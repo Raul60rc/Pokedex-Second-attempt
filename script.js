53813691a9807bd9d15dc333456648b452812097
Raul60rc <34682876+Raul60rc@users.noreply.github.com>
2023-01-23 23:03:19 +0100

    added JScript,queryslectors & colors array remaining

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// creating query selectors so we can add CSS
+const pokeCard = document.querySelector("[data-poke-card]");
+const pokeName = document.querySelector("[data-poke-name]");
+const pokeImg = document.querySelector("[data-poke-img]");
+const pokeImgContainer = document.querySelector("[data-poke-img-container]");
+const pokeId = document.querySelector("[data-poke-id]");
+const pokeTypes = document.querySelector("[data-poke-types]");
+const pokeStats = document.querySelector("[data-poke-stats]");
 
+// creating a array of colors for pokemon stats 
+const typeColors ={
+
+}

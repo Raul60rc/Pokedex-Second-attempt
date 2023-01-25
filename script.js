8684078fac009bfae4b89eb32f83f7c65407de59
Raul60rc <34682876+Raul60rc@users.noreply.github.com>
2023-01-25 23:22:27 +0100

    need to fix the fetch & catch error

--- script.js
+++ script.js
@@ -9,5 +9,43 @@ const pokeStats = document.querySelector("[data-poke-stats]");
 
 // creating a array of colors for pokemon stats 
 const typeColors ={
+    electric: '#FFEA70',
+    normal: '#B09398',
+    fire: '#FF675C',
+    water: '#0596C7',
+    ice: '#AFEAFD',
+    rock: '#999799',
+    flying: '#7AE7C7',
+    grass: '#4A9681',
+    psychic: '#FFC6D9',
+    ghost: '#561D25',
+    bug: '#A2FAA3',
+    poison: '#795663',
+    ground: '#D2B074',
+    dragon: '#DA627D',
+    steel: '#1D8A99',
+    fighting: '#2F2F2F',
+    default: '#2A1A1F',
+};
 
+// search bar functionality
+const searchPokemon = event =>{
+    event.preventDefault(); 
+    const { value } = event.target.pokemon
+    fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
+    .then(data => data.json())
+    .then(response => renderPokemonData(response))
+    .catch(err => renderNotFound())
+}
+
+const renderPokemonData = data => {
+    const sprite = data.sprites.font_default;
+    const { stats, types } = data;
+
+    pokeName.textContent = data.pokeName;
+    pokeImg.setAttribute('src',sprite);
+    pokeId.textContent= `Nº ${data.id}`;
+    setCardColor(types);
+    renderPokemonTypes(types);
+    renderPokemonStats(stats);
 }

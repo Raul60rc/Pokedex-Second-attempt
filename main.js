685a29bf7bbe9923b8995335b8762c522b9f95f4
Raul60rc <34682876+Raul60rc@users.noreply.github.com>
2023-02-04 16:48:42 +0100

    fetch added for pokemons in js

---
+++ main.js
@@ -0,0 +1,8 @@
+const pokemonContainer = document.querySelector(".pokemon-container")
+
+function fetchPokemon(id){
+    fetch('https://pokeapi.co/api/v2/pokemon/${id}/')
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+fetchPokemon(1)

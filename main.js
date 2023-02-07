3140b3e8ef244bcb5ed80b3b77d2c589f9f0f4cd
Raul60rc <34682876+Raul60rc@users.noreply.github.com>
2023-02-07 15:03:32 +0100

    fetchPokemons fixed need to show on html now

--- main.js
+++ main.js
@@ -1,8 +1,45 @@
-const pokemonContainer = document.querySelector(".pokemon-container")
+const pokemonContainer = document.querySelector('.pokemon-container')
+
 
 function fetchPokemon(id){
-    fetch('https://pokeapi.co/api/v2/pokemon/${id}/')
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => console.log(data)) 
+    };
+
+function fetchPokemons(number){
+ for (let index = 1; index < number; index++) {
+    fetchPokemon(index)
+    }
 }
-fetchPokemon(1)
+
+
+function createPokemon(pokemon){ //DOM 
+    const card = document.createElement("div");
+    card.classList.add('pokemon-block');
+
+    const spriteContainer = document.createElement('div'); // creates a div for every sprite container will contain image
+    spriteContainer.classList.add('img-container')
+
+    const sprite = document.createElement('img')
+    sprite.src = pokemon.sprites.front_default // it has this property in the sprite
+
+    spriteContainer.appendChild(sprite);  
+
+    const number = document.createElement('p');
+    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`; //
+
+    const name = document.createElement('p');
+    name.classList.add ('name');
+    textContent = pokemon.name;
+
+    card.appendChild(spriteContainer);
+    card.appendChild(number);
+    card.appendChild(name);
+
+    pokemonContainer.appendChild(card);
+    //appenchild adds DOM to html 
+
+}
+
+fetchPokemons(9);

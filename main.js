a5f6fa4e56ca5645226c995ec203419f0fc41731
Raul60rc <34682876+Raul60rc@users.noreply.github.com>
2023-02-12 16:11:17 +0100

    colours added to pokemon classes

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 const pokemonContainer = document.querySelector('.pokemon-container')
 const spinner = document.querySelector('#spinner')
 
+const typeColors ={
+    electric: '#FFEA70',
+    normal: '#B09398',
+    fire: '#FF675C',
+    water: '#0596C7',
+    ice: '#AFEAFD',
+    rock: '#999799',
+    flying: '#7AE7C7',
+    grass: '#4A9681',
+    psychic: '#FFC6D9',
+    ghost: '#561D25',
+    bug: '#A2FAA3',
+    poison: '#795663',
+    ground: '#D2B074',
+    dragon: '#DA627D',
+    steel: '#1D8A99',
+    fighting: '#2F2F2F',
+    default: '#2A1A1F',
+};
+
 let offset = 1;
 let limit = 8;
 
@@ -14,9 +34,9 @@ function fetchPokemon(id){
     }
     )};
 
-function fetchPokemons(offset,limit) {
+function fetchPokemons(number) {
     spinner.style.display = "block";
- for (let index = offset; index < offset; limit++) {
+ for (let index = 1; index < number; index++) {
     fetchPokemon(index)
     }
 }
@@ -50,4 +70,4 @@ function createPokemon(pokemon){ //DOM
 
 }
 
-fetchPokemons(offset,limit);
+fetchPokemons(152);
